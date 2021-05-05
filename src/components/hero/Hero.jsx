024e359866a9ca8d8dc639dfc9ecfc692c58486e
Warz66 <warzox@hotmail.com>
2021-05-05 22:30:14 +0200

    hero commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,17 +1,25 @@
 import './Hero.css';
+import cv from 'assets/rsc/CVOeslickDavid.pdf'
+import keanu from 'assets/images/matrix-keanu-reeves.png'
 
 const Hero = () => {
 
     return (
 
         <section id="hero">
-            
-            <div id="hero-text">
-                <span>BONJOUR</span>
-                <h1>Je suis <span>David</span> Oeslick</h1>
-                <p>Développeur Web Front-end / Symfony</p>
+            <div id="wrapper-hero">
+                <div id="hero-text">
+                    <span>BONJOUR</span>
+                    <h1>Je suis <span>David</span> Oeslick</h1>
+                    <p>Développeur Web Front-end / Symfony</p>
+                    <a href={cv} download="CVOeslickDavid.pdf" id="btn-dl-cv">
+                        DOWNLOAD CV
+                    </a>
+                </div>
+                <div id="hero-img">
+                    <img src={keanu} alt=""/>
+                </div>
             </div>
-
         </section>
 
     );
