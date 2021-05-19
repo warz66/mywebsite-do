01520a55625ae78fb5b6fbb031f7ae7a99dfe680
Warz66 <warzox@hotmail.com>
2021-05-19 23:40:31 +0200

    realisations commit

--- src/components/realisations/Realisations.jsx
+++ src/components/realisations/Realisations.jsx
@@ -1,4 +1,3 @@
-import './Realisations.css'
 import Flickity from 'react-flickity-component'
 import archRealisation from 'assets/images/arch-realisation.jpg'
 import photosnapRealisation from 'assets/images/photosnap-realisation.jpg'
@@ -7,14 +6,17 @@ import mairieRealisation from 'assets/images/mairie-realisation.jpg'
 import clipboardRealisation from 'assets/images/clipboard-realisation.jpg'
 import roomRealisation from 'assets/images/room-realisation.jpg'
 import 'flickity/dist/flickity.min.css'
+import './Realisations.css'
 
 const Realisations = () => {
 
     const flickityOptions = {
+        /*freeScroll: true,*/
+        contain: true,
         initialIndex: 0,
         groupCells: true,
         prevNextButtons: false,
-        pageDots: false,
+        pageDots: true,
         watchCSS: true
     }
 
@@ -33,12 +35,13 @@ const Realisations = () => {
                     static // default false
                 >
                     <div className="card-realisation">
-                       <img className="" src={archRealisation} alt="" />
-                       <div className="card-realisation-text">
+                        <img className="" src={archRealisation} alt="" />
+                        <div className="card-realisation-text">
                             <div className="card-realisation-tag">React</div>
                             <h4>Arch Studio multi-page website</h4>
                             <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={photosnapRealisation} alt="" />
@@ -47,6 +50,7 @@ const Realisations = () => {
                             <h4>Photosnap multi-page website</h4>
                             <p>In this challenge, you'll be building the marketing site for a photo-sharing app. This will be a perfect opportunity to put your CSS Grid skills to the test!</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={countriesRealisation} alt="" />
@@ -55,6 +59,7 @@ const Realisations = () => {
                             <h4>REST Countries API with color theme switcher</h4>
                             <p>If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={countriesRealisation} alt="" />
@@ -63,6 +68,7 @@ const Realisations = () => {
                             <h4>REST Countries API with color theme switcher</h4>
                             <p>If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={mairieRealisation} alt="" />
@@ -71,6 +77,7 @@ const Realisations = () => {
                             <h4>Web Solution Mairie</h4>
                             <p>Solution pour site institutionnel</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={clipboardRealisation} alt="" />
@@ -79,6 +86,7 @@ const Realisations = () => {
                             <h4>Arch Studio multi-page website</h4>
                             <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={roomRealisation} alt="" />
@@ -87,6 +95,7 @@ const Realisations = () => {
                             <h4>Arch Studio multi-page website</h4>
                             <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                     <div className="card-realisation">
                        <img className="" src={archRealisation} alt="" />
@@ -95,6 +104,7 @@ const Realisations = () => {
                             <h4>Arch Studio multi-page website</h4>
                             <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
                         </div>
+                        <a href="" className="card-realisation-more">En savoir plus</a>
                     </div>
                 </Flickity>
             </div> 
