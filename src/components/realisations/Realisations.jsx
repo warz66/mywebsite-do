7b5988d071b91f8233ba183480d1b8a5a882daaa
Warz66 <warzox@hotmail.com>
2021-05-20 02:44:21 +0200

    realisations commit

--- src/components/realisations/Realisations.jsx
+++ src/components/realisations/Realisations.jsx
@@ -1,10 +1,6 @@
 import Flickity from 'react-flickity-component'
-import archRealisation from 'assets/images/arch-realisation.jpg'
-import photosnapRealisation from 'assets/images/photosnap-realisation.jpg'
-import countriesRealisation from 'assets/images/countries-realisation.jpg'
-import mairieRealisation from 'assets/images/mairie-realisation.jpg'
-import clipboardRealisation from 'assets/images/clipboard-realisation.jpg'
-import roomRealisation from 'assets/images/room-realisation.jpg'
+import portfolioRealisations from './portfolioRealisations';
+import Realisation from 'components/realisations/realisation/realisation';
 import 'flickity/dist/flickity.min.css'
 import './Realisations.css'
 
@@ -33,79 +29,10 @@ const Realisations = () => {
                     disableImagesLoaded={false} // default false
                     reloadOnUpdate // default false
                     static // default false
-                >
-                    <div className="card-realisation">
-                        <img className="" src={archRealisation} alt="" />
-                        <div className="card-realisation-text">
-                            <div className="card-realisation-tag">React</div>
-                            <h4>Arch Studio multi-page website</h4>
-                            <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={photosnapRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">Vue</div>
-                            <h4>Photosnap multi-page website</h4>
-                            <p>In this challenge, you'll be building the marketing site for a photo-sharing app. This will be a perfect opportunity to put your CSS Grid skills to the test!</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={countriesRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">React</div>
-                            <h4>REST Countries API with color theme switcher</h4>
-                            <p>If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={countriesRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">Vue</div>
-                            <h4>REST Countries API with color theme switcher</h4>
-                            <p>If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={mairieRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">Symfony</div>
-                            <h4>Web Solution Mairie</h4>
-                            <p>Solution pour site institutionnel</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={clipboardRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">HTML / CSS</div>
-                            <h4>Arch Studio multi-page website</h4>
-                            <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={roomRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">HTML / CSS / JS</div>
-                            <h4>Arch Studio multi-page website</h4>
-                            <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
-                    <div className="card-realisation">
-                       <img className="" src={archRealisation} alt="" />
-                       <div className="card-realisation-text">
-                            <div className="card-realisation-tag">React</div>
-                            <h4>Arch Studio multi-page website</h4>
-                            <p>In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.</p>
-                        </div>
-                        <a href="" className="card-realisation-more">En savoir plus</a>
-                    </div>
+                >   
+                    {portfolioRealisations.map( (realisation, index) => {
+                            return <Realisation realisation={realisation} key={index}/> 
+                    })}
                 </Flickity>
             </div> 
         </section>
---
+++ src/components/realisations/portfolioRealisations.js
@@ -0,0 +1,60 @@
+import archRealisation from 'assets/images/arch-realisation.jpg'
+import photosnapRealisation from 'assets/images/photosnap-realisation.jpg'
+import countriesRealisation from 'assets/images/countries-realisation.jpg'
+import mairieRealisation from 'assets/images/mairie-realisation.jpg'
+import clipboardRealisation from 'assets/images/clipboard-realisation.jpg'
+import roomRealisation from 'assets/images/room-realisation.jpg'
+
+const portfolio = [
+    {   
+        title: "Arch Studio multi-page website",
+        image: archRealisation,
+        techs: "React",
+        sum: "In this challenge, you'll be building a 4-page site that will look great in any portfolio. You'll even get the chance to play with mapping APIs if you choose the bonus.",
+        path: "",
+    },
+    {   
+        title: "Photosnap multi-page website",
+        image: photosnapRealisation,
+        techs: "Vue",
+        sum: "In this challenge, you'll be building the marketing site for a photo-sharing app. This will be a perfect opportunity to put your CSS Grid skills to the test!",
+        path: "",
+    },
+    {   
+        title: "REST Countries API with color theme switcher",
+        image: countriesRealisation,
+        techs: "React",
+        sum: "If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.",
+        path: "",
+    },
+    {   
+        title: "REST Countries API with color theme switcher",
+        image: countriesRealisation,
+        techs: "Vue",
+        sum: "If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.",
+        path: "",
+    },
+    {   
+        title: "Web Solution Mairie",
+        image: mairieRealisation,
+        techs: "Symfony",
+        sum: "Solution pour site institutionnel.",
+        path: "",
+    },
+    {   
+        title: "Clipboard landing page",
+        image: clipboardRealisation,
+        techs: "HTML / CSS",
+        sum: "Put your layout skills to the test with this HTML & CSS landing page challenge. This challenge includes a design for hover states.",
+        path: "",
+    },
+    {   
+        title: "Room homepage",
+        image: roomRealisation,
+        techs: "HTML / CSS / JS",
+        sum: "This small homepage challenge packs a big punch to test your layout skills. There's also a slider in there to add a JS layer for extra practice.",
+        path: "",
+    },
+]
+
+export default portfolio;
