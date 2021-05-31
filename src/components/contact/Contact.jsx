162f2553ace874ad43a1a547aa3ed0cd0dbb33c5
Warz66 <warzox@hotmail.com>
2021-05-31 19:14:45 +0200

    contact commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,26 @@ const Contact = () => {
 
     return (
 
-        <section id="contact" className="section bg-vr">
-            
+        <section id="contact" className="section">
+            <div id="wrapper-contact">
+                <div id="form-contact">
+
+                </div>
+                <div id="lol">
+                    <div id="msg-contact">
+                        Let's make something new, different and more meanningful or make thing more visual or Conceptual ? Just Say Hello !
+                    </div>
+                    <div id="info-contact">
+                        - sdf sdf sdfsdf sdf s <br />
+                        - sdf sdf sdfsdf sdf s <br />
+                        - sdf sdf sdfsdf sdf s <br />
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <p>Copyright © 2021 xxxxxx, All rights Reserved.</p>
+                <p>Created by xxxxxx</p>
+            </footer>
         </section>
 
     );
