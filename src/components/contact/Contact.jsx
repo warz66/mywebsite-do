490eee99080d54b883d4e29c121ef4bb0f4ebfd9
Warz66 <warzox@hotmail.com>
2021-06-04 15:30:55 +0200

    contact commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,20 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import './Contact.css'
 
 const Contact = () => {
+    const [bgMode, setBgMode] = useState(true);
+
+    useEffect(() => {
+        
+        function handleBg() {
+            if(window.innerWidth > 1200) {
+                setBgMode(true);
+            } else {
+                setBgMode(false); 
+            }
+        }
+        handleBg();
+
+        /*function handleFormBg() {
+            if(window.innerWidth > 1100) {
+                setBgMode(true);
+            } else {
+                setBgMode(false); 
+            }
+        }*/
+
+        window.addEventListener('resize', handleBg);
+        return () => {
+            window.removeEventListener('resize', handleBg);
+        }
+    });
 
     return (
 
         <section id="contact" className="section">
-            <div id="wrapper-contact">
-                <div id="form-contact">
-
-                </div>
-                <div id="wrapper-msg-info-contact">
-                    <div id="msg-contact">
+            <div id="wrapper-contact" className={bgMode ? "" : "bg-vr"}>
+                <div className={bgMode ? "bg-dark" : ""}>
+                    <div id="form-msg-contact">
                         <p>
                             Let's make something new, different and more meanningful or make thing more visual or Conceptual ? <span>Just Say Hello !</span>
                         </p>
                     </div>
+                </div>
+                <div>
+                    <div id="form-contact">
+
+                    </div>
+                </div>
+                <div className={bgMode ? "bg-vr" : ""}>
                     <div id="info-contact">
                         <p>0655894578</p>
                         <p>myMail@mail.com</p>
@@ -22,7 +53,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <footer>
+            <footer className="bg-dark">
                 <p>Copyright © 2021 Freelance_Theme, All rights Reserved.</p>
                 <p>Created by DavidO_Design</p>
             </footer>
---
+++ src/components/contact3/Contact.jsx
@@ -0,0 +1,35 @@
+import './Contact.css'
+
+const Contact = () => {
+
+    return (
+
+        <section id="contact" className="section">
+            <div id="wrapper-contact">
+                <div id="form-contact">
+
+                </div>
+                <div id="wrapper-msg-info-contact">
+                    <div id="msg-contact">
+                        <p>
+                            Let's make something new, different and more meanningful or make thing more visual or Conceptual ? <span>Just Say Hello !</span>
+                        </p>
+                    </div>
+                    <div id="info-contact">
+                        <p>0655894578</p>
+                        <p>myMail@mail.com</p>
+                        <p>Cap Luberon</p>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <p>Copyright © 2021 Freelance_Theme, All rights Reserved.</p>
+                <p>Created by DavidO_Design</p>
+            </footer>
+        </section>
+
+    );
+
+}
+    
+export default Contact;
