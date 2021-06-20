6d6732d4bf7eb8cd4513bf96f857e8095f3d9e40
Warz66 <warzox@hotmail.com>
2021-06-21 00:31:38 +0200

    views realisation commit

--- src/views/realisation/Realisation.jsx
+++ src/views/realisation/Realisation.jsx
@@ -8,45 +8,47 @@ import realisationsMap from 'assets/realisations/realisationsMap';
 
 const Realisation = ({mode, changeMode, handleStyleFpNav}) => {
     let { slug } = useParams();
-    const [isLoaded, setIsLoaded] = useState(false);
+    //const [isLoaded, setIsLoaded] = useState(false);
     const [errorMapSlug, setErrorMapSlug] = useState(false);
-    let realisation = null;
+    const [realisation, setRealisation] = useState(false);
+    const [anchors, setAnchors] = useState(["PRESENTATION", "CONTACT"]);
 
     function importRealisation() {
         realisationsMap.map((realisationMap, i) => {
-            if(realisationMap.slug === slug && !isLoaded) {
+            if(realisationMap.slug === slug && !realisation) {
                 import("assets/realisations/"+realisationMap.path).then( data => {
-                    realisation = data.default[0]; setIsLoaded(true); console.log(realisation.title);
+                    setTimeout(function(){ setRealisation(data.default[0]); setAnchors(["PRESENTATION" ,"FONCTIONALITES" , "CONTACT"]);}, 3000); /*setIsLoaded(true);*/ /*console.log(realisation.title);*/
                 }).catch((err) => {console.log(err); setErrorMapSlug(true);} ); 
-            } else if(realisationsMap.length === i+1 && isLoaded === false) {
+            } else if(realisationsMap.length === i+1 && !realisation) {
                 setErrorMapSlug(true);
             }
         });
     }
 
+    console.log(realisation);
+    
     function RealisationLazy() {
         if (!errorMapSlug) {
-            if(!isLoaded) {
-                return <div className="section">Loading...</div>
+            if(!realisation) {
+                return (
+                    <div className="section">Loading...</div>
+                );
             } else {
                 return (
-                    <div>
+                    <>
                         <div className="section">
-                            <h2></h2>
+                            <h2>{realisation.title}</h2>
                         </div>
                         <div className="section">
-
+                            <h2>{realisation.title}</h2>
                         </div>
-                    </div>
+                    </>
                 );
             }
         } else {
             return <div className="section">Nous n'avons pas trouvé la réalisation {slug}.</div>
         }
     }
-
-    const anchors = ["PRESENTATION" ,"FONCTIONALITES" , "CONTACT"];
-    //anchors.splice(1, 0, "FUNCTIONALITE P1", "FUNCTIONALITE P2");
 
     const RealisationView = () => (
 
@@ -64,11 +66,13 @@ const Realisation = ({mode, changeMode, handleStyleFpNav}) => {
     
                 return (
                     <main>
+
                         <Header/>
 
                         <RealisationLazy/>
 
                         <Contact/>
+
                     </main>
                 );
             }}
@@ -78,7 +82,7 @@ const Realisation = ({mode, changeMode, handleStyleFpNav}) => {
     useEffect(() => {
         handleStyleFpNav();
         importRealisation();
-    }, [isLoaded]);
+    }, [realisation, anchors, errorMapSlug]);
 
     return (
 
