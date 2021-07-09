02e58ae84f4163f4974d798647b515e08f1f7785
Warz66 <warzox@hotmail.com>
2021-07-09 06:50:54 +0200

    realisation features commit

--- src/components/realisation-features/realisation-feature/RealisationFeature.jsx
+++ src/components/realisation-features/realisation-feature/RealisationFeature.jsx
@@ -4,9 +4,6 @@ const RealisationFeature = ({feature}) => {
 
     return(
         <div className="realisation-feature">
-            {/*<div className="realisation-feature-title">
-                <h4>{feature.title}</h4>
-            </div>*/}
             <div className="realisation-feature-text">
                 <h4>{feature.title}</h4>
                 {feature.text.map( (paragraphe, index) => {
@@ -14,13 +11,12 @@ const RealisationFeature = ({feature}) => {
                 })}
             </div>
             <div className="realisation-feature-img">
-            <div className="realisation-feature-link">
+                <div className="realisation-feature-link">
                     {feature.linkGithub && <a href={feature.linkGithub} target="_blank" rel="noreferrer">Lien github</a>}
                     {feature.linkGithub && feature.linkWebsite && <span>/</span>}
                     {feature.linkWebsite && <a href={feature.linkWebsite} target="_blank" rel="noreferrer">Voir le site</a>}
                 </div>
-                <img className="" src={feature.image} alt=""/>
-                
+                <img src={feature.image} alt=""/> 
             </div>
         </div>
     );
