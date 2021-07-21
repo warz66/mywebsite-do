0951bd8d3222d7a64dd385168bc12940168648f3
Warz66 <warzox@hotmail.com>
2021-07-21 23:26:26 +0200

    update .env

--- src/components/switch-mode/SwitchMode.jsx
+++ src/components/switch-mode/SwitchMode.jsx
@@ -8,8 +8,6 @@ const SwitchMode = ({text = true}) => {
     const mode = useSelector((state) => state.mode.value);
     const animOK = useSelector((state) => state.mode.animBtn);
     const dispatch = useDispatch();
-
-    console.log('lol');
     
     function changeMode() {
         dispatch(resetSection());
