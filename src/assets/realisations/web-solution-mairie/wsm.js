f9e5c1f11ba394dd47b77a2898ac6b6ca1f9969b
Warz66 <warzox@hotmail.com>
2021-11-03 17:20:34 +0100

    update realisation specs web solution mairie

--- src/assets/realisations/web-solution-mairie/wsm.js
+++ src/assets/realisations/web-solution-mairie/wsm.js
@@ -12,8 +12,8 @@ const wsm = [
         techs: "SYMFONY 5, JQUERY, BOOTSTRAP 4",
         sum: "Solution pour sites de partage d'informations pour les collectivités locales, communes.",
         presentation: [
-            "Projet personnel ayant pour but d'apprendre le framework Symfony 5. Cela m'a permis d'appréhender le champ de développement d'une application web dans son ensemble, du front-end au back-end jusqu'à la mise en production. Symfony nous donne un cadre de travail (règles métier, architecture, composants) afin d'être plus efficace, de se concentrer sur l'essentiel et de produire un code de meilleure qualité. Le monde du dev web peut-être déboussolant par la multitude de technologies qu'il comporte, Symfony nous dirige, nous rassure et nous conforte dans notre travail. Les formations vidéo de <a href='https://www.youtube.com/c/LiorCHAMLA' target='_blank' rel='noreferrer'>Lior Chamla</a>, de <a href='https://www.youtube.com/c/grafikart/featured' target='_blank' rel='noreferrer'>Grafikart</a>, ainsi que les cours  <a href='https://openclassrooms.com/fr/' target='_blank' rel='noreferrer'>OpenClassRooms</a> m'ont beaucoup aidé en dehors de la documentation officiel.",
-            "Pour la plupart des problématiques d'un projet, un CMS tel que Wordpress, Drupal et bien d'autres peuvent largement convenir, néanmoins, j'avais envie de coller au plus près de la conception de ce que peut-être un CMS en créant une solution sur-mesure qui répondrait aux besoins 'précis' des collectivités. Cette solution ne vise que les communes de moins de 30 000 habitants qui représente la majorité (99 %) de l'ensemble des communes de France. Ce sont surtout des sites qui permettent le partage des informations relatives à la vie de la commune, sous forme d'actualités, d'événements ou de pages informatives. Des fonctionnalités me paraissaient essentielles, tel qu'un carousel sur la homepage, un 'flash infos', un kiosque de diffusion de documents, des liens rapides vers du contenu essentiels, une barre de recherche, des galeries d'images, une newsletter, un formulaire de contact, et bien évidemment un back-office accessible à des néophytes.",
+            "Projet personnel ayant pour but d'apprendre le framework Symfony 5. Cela m'a permis d'appréhender le développement d'une application web dans son ensemble, du front-end au back-end jusqu'à la mise en production. Le monde du dev web peut-être anarchique et déboussolant par sa multitude de technologies. Symfony nous dirige, nous rassure et nous conforte en nous donnant un cadre de travail (règles métier, architecture, composants, choix techniques) afin d'être plus efficace, de se concentrer sur l'essentiel et de produire un code de meilleure qualité. Les formations vidéo de <a href='https://www.youtube.com/c/LiorCHAMLA' target='_blank' rel='noreferrer'>Lior Chamla</a>, de <a href='https://www.youtube.com/c/grafikart/featured' target='_blank' rel='noreferrer'>Grafikart</a>, ainsi que les cours  <a href='https://openclassrooms.com/fr/' target='_blank' rel='noreferrer'>OpenClassRooms</a> m'ont beaucoup aidé en dehors de la documentation officiel.",
+            "Pour la plupart des problématiques d'un projet, un CMS tel que Wordpress, Drupal et bien d'autres peuvent largement convenir, néanmoins, j'avais envie de coller au plus près de la conception de ce que peut-être un gestionnaire de contenu en créant une solution sur-mesure qui répondrait aux besoins 'précis' des collectivités. Cette solution vise essentiellement les communes de moins de 30 000 habitants (99% en France). Elles ont besoins de partager des informations sous forme d'actualités, d'événements ou de pages informatives. Des fonctionnalités, 'modules', me paraissaient essentielles, tel qu'un carrousel, un 'flash infos', un kiosque de documentation, des liens rapides vers du contenu essentiels, une barre de recherche, des galeries d'images, une newsletter, un formulaire de contact, un menu, des statistiques Google Analytics et bien évidemment un back-office sécurisé avec gestion de droit, accessible à des néophytes.",
             "Vous pouvez tester le back office de la solution sur <a href='https://web-solution-mairie.fr/admin/login' target='_blank' rel='noreferrer'>web-solution-maire.fr/admin/login</a> avec les identifiants: invite@mairie.com password"
         ],
         linkGithub: "https://github.com/warz66/Solution-mairie-symfony-5",
@@ -28,30 +28,96 @@ const wsm = [
                 sections: [
                     {
                         paragraphe: [
-                            "Le principal besoin de cette solution est de pouvoir publier du contenu hierarchisés, catégorifiés et libellisés. Je suis parti du principe que tout contenu est une publication qui peut avoir différentes natures: actualité, évènement, page, sous-rubrique, rubrique.",
-                            "Pour le contenu intemporel, j'ai voulu créer une arborescence en 3 couches maximum (rubriques -> sous-rubriques -> pages) pour une navigation plus ergonomique. La plupart des CMS laisse la possiblité de créer des noeuds d'arborescences illimités, néamoins j'ai trouvé ça plus clair et simpliste de le limiter. On retrouve les rubriques dans le menu, les sous-rubriques pour affiner l'information, puis les pages qui est le contenu fixe dont l'internaute à besoin.", 
-                            "Une actualité et un évènement sont deux type de contenu que l'on a envie de présenter de manières différentes de par leur nature temporel.",
-                            "On peut aussi libelliser ('tags') le contenu , afin de regrouper l'informations pour une meilleure expérience.",
-                            "Se greffe aussi, selon la nature de la publication des fonctionalités qui permettent de rajouter des informations pertinentes à mettre en avant, telle que des liens utiles (lien-hypertexte), des documents (pdf), des infos pratiques, une ou des galeries d'images."
+                            "La principale fonctionalité de cette solution est de pouvoir éditer et publier du contenu hiérarchisé, catégorisé et libellisé. Je suis parti du principe que tout contenu est une publication qui peut avoir différentes natures : actualité, événement, page, sous-rubrique, rubrique.",
+                            "Pour le contenu fixe, j'ai voulu créer une arborescence en 3 couches maximum (rubriques -> sous-rubriques -> pages) pour une navigation plus ergonomique. La plupart des CMS laissent la possibilité de créer des noeuds d'arborescences illimités, néanmoins, j'ai trouvé cela plus clair et efficace de les limiter. Dans le menu on retrouve les rubriques, les sous-rubriques qui affine la recherche, puis les pages contenant l'information dont l'internaute à besoin.",
+                            "Une actualité et un événement sont deux types de contenu présentés de manière différente de par leur nature temporelle.",
+                            "On peut aussi libelliser ('tags') le contenu, afin de regrouper l'information pour une meilleure expérience.",
+                            "Se greffe aussi, selon la nature de la publication des fonctionnalités qui permettent de rajouter des informations pertinentes à mettre en avant, telle que des liens utiles (lien-hypertexte), des documents (pdf), des infos pratiques, une ou des galeries d'images."
                         ]
                     },
                 ],
                 linksGithubs: [
                     {
-                        href: "https://github.com/warz66/next-sven-rybin/blob/main/pages/index.js",
-                        title: "Homepage structure"
-                    },
-                    {
-                        href: "https://github.com/warz66/next-sven-rybin/blob/main/components/home/galeries/galerie/Galerie.jsx",
-                        title: "Exemple utilisation composant Image de next"
-                    },
-                    {
-                        href: "https://github.com/warz66/next-sven-rybin/blob/main/pages/galerie/index.jsx",
-                        title: "Exemple getServerSideProps"
+                        href: "https://github.com/warz66/Solution-mairie-symfony-5/tree/main/src/Entity",
+                        title: "Dossier entité du projet"
                     },
                 ],
                 linkWebsite: {
-                    href: "https://www.svenrybin.fr",
+                    href: "https://web-solution-mairie.fr",
+                    title: "Voir la Homepage"
+                }
+            },
+            {
+                title: "Modules et fonctionalités (Partie 1)",
+                image: {
+                    thumbnail: bddRelationTableTn,
+                    large: bddRelationTable,
+                },
+                sections: [
+                    {   
+                        title: "Le carrousel",
+                        paragraphe: [
+                            "La page d’accueil d’une commune concentre pas mal de données. Le carrousel à l'avantage de rassembler plusieurs types de contenu différents et de le mettre en évidence sans noyer l’internaute d’informations. J'ai limité le nombre de 'slide' à 5 pour une question d'Ux. J'ai donné la possibilité de donner un titre, un texte, une image, et de les lier à n'importe qu'elle type de publication.",
+                        ]
+                    },
+                    {   
+                        title: "Le flash infos",
+                        paragraphe: [
+                            "Il permet d'attirer l'attention ou d'alerter rapidement sur une information précise. Il doit-être placé de préférence sur le haut de la homepage. Il peut-être lié à une publication ou à un lien externe.",
+                        ]
+                    },
+                    {   
+                        title: "L'accès rapide",
+                        paragraphe: [
+                            "Il synthétise un ensemble de raccourcis vers des pages à contenu pratique pour l'internaute. Ces raccourcis caractérisé par une icône et un nom ce démarque en attirant l'oeil instinctivement. La navigation s'effectue en slidant. J'ai utilisé <a href='https://kenwheeler.github.io/slick/' target='_blank' rel='noreferrer'>'Slick'</a> pour sa structure et sa navigation et pour le choix de l'icône <a href='https://victor-valencia.github.io/bootstrap-iconpicker/' target='_blank' rel='noreferrer'>'Bootstrap-Iconpicker'</a>.",
+                        ]
+                    },
+                ],
+                linksGithubs: [
+                    {
+                        href: "https://github.com/warz66/Solution-mairie-symfony-5/tree/main/src/Entity",
+                        title: "Dossier entité du projet"
+                    },
+                ],
+                linkWebsite: {
+                    href: "https://web-solution-mairie.fr",
+                    title: "Voir la Homepage"
+                }
+            },
+            {
+                title: "Modules et fonctionalités (Partie 2)",
+                image: {
+                    thumbnail: bddRelationTableTn,
+                    large: bddRelationTable,
+                },
+                sections: [
+                    {   
+                        title: "Le kiosque",
+                        paragraphe: [
+                            "Il permet de partager des documents pdf. J'ai utilisé la même méthode de navigation que pour l'accès-rapide. Chaque document est représenté visuellement par sa première page (<a href='https://github.com/imalhasaranga/PDFLib' target='_blank' rel='noreferrer'>imalhasarange/PDFLib</a>) ainsi que d'un titre et de son poids (taille ko).",
+                        ]
+                    },
+                    {   
+                        title: "La barre de recherche",
+                        paragraphe: [
+                            "",
+                        ]
+                    },
+                    {   
+                        title: "Le formulaire de contact",
+                        paragraphe: [
+                            "",
+                        ]
+                    },
+                ],
+                linksGithubs: [
+                    {
+                        href: "https://github.com/warz66/Solution-mairie-symfony-5/tree/main/src/Entity",
+                        title: "Dossier entité du projet"
+                    },
+                ],
+                linkWebsite: {
+                    href: "https://web-solution-mairie.fr",
                     title: "Voir la Homepage"
                 }
             },
