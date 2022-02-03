63054dafa85bccbcde168ad1f9065a3398756cbe
Warz66 <warzox@hotmail.com>
2022-02-03 16:54:16 +0100

    debug tanslate realisations cards

--- src/components/realisations/realisation-card/RealisationCard.jsx
+++ src/components/realisations/realisation-card/RealisationCard.jsx
@@ -5,12 +5,10 @@ const RealisationCard = ({realisation}) => {
 
     return (
         <div className="realisation-card">
-            <img src={realisation.image} alt=""/>
-            {/*<Link to={{ pathname: "/realisation/"+realisation.slug }}><img src={realisation.image} alt=""/></Link>*/}
+            <Link to={{ pathname: "/realisation/"+realisation.slug }}><img src={realisation.image} alt=""/></Link>
             <div className="realisation-card-text">
                 <div className="realisation-card-tag">{realisation.techs}</div>
-                <h4>{realisation.title}</h4>
-                {/*<Link to={{ pathname: "/realisation/"+realisation.slug }}><h4>{realisation.title}</h4></Link>*/}
+                <Link to={{ pathname: "/realisation/"+realisation.slug }}><h4>{realisation.title}</h4></Link>
                 <p>{realisation.sum}</p>
             </div>
             <Link to={{ pathname: "/realisation/"+realisation.slug }} className="realisation-card-more">En savoir plus</Link>
